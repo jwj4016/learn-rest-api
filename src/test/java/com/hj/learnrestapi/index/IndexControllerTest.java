00f583fb3e11c25773055f162099a075026ebafd
j <jwj4016@gmail.com>
2023-12-09 20:14:34 +0900

    api index 를 위한 컨트롤러 및 테스트 생성.

---
+++ src/test/java/com/hj/learnrestapi/index/IndexControllerTest.java
@@ -0,0 +1,33 @@
+package com.hj.learnrestapi.index;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ActiveProfiles("test")
+public class IndexControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void index() throws Exception {
+        this.mockMvc.perform(get("/api"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("_links.events").exists());
+    }
+}
